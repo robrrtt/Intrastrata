c0605bd8d3c214b1e44d1d54db95e9b4ba1307d9
robrrtt <janipin0610@gmail.com>
2025-02-17 16:33:15 +0800

    attachmentEmail

--- routes/web.php
+++ routes/web.php
@@ -118,4 +118,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
 require __DIR__.'/auth.php';
